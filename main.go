8091bf28dcd6d8e928b7db1a7491e1b42a0a2152
Sanzhar Anarbay <anarbay.sanzhar1999@gmail.com>
2021-02-20 16:24:51 +0600

    Struct Go

--- main.go
+++ main.go
@@ -1,19 +1,58 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type Person struct {
+	Id int
+	Name string
+}
+
+func (p *Person) SetName(name string){
+	p.Name = name
+}
+
+type Account struct{
+	Id int
+	Name string
+	Person
+}
+
+type MySlice []int
+
+func (mSl *MySlice) Add(val int)  {
+	*mSl = append(*mSl, val)
+}
+
+func (mSl *MySlice) Count() int  {
+	return len(*mSl)
+}
 
 func main() {
+
+	pers:= Person{1, "Sanzhar"}
+	pers.SetName("Sanzh")
+
+	var account Account = Account{
+		Id: 1,
+		Name: "Dl",
+		Person: Person{
+			Id: 2,
+			Name: "Sanzhar Anarbay",
+		},
+	}
+
+	account.SetName("Ivan Ivanov")
+
+	fmt.Println(account)
+
+	mSl := MySlice([]int{1,2})
+
+	mSl.Add(3)
+
+	fmt.Println(mSl)
+	fmt.Println(mSl.Count())
+
 	var buff []int
-	buf1 := []int{}
-	bif2 := []int{42}
-	buf3 := make([]int, 0)
-	buf4 := make([]int, 5)
-	buf5 := make([]int, 5, 10)
-
-	fmt.Println(buff, buf1, bif2, buf3, buf4, buf5)
-
 	buff = append(buff, 9, 10)
 	buff = append(buff, 13)
 
@@ -82,4 +121,7 @@ func main() {
 	for _, value := range user {
 		fmt.Println("Range map by value", value)
 	}
+
+
+
 }
